311cd7b7c48ec2fbf4d56a607b85992b06cb4e5d
Kranthi Kumar Pasala <Kranthi_Kumar_Pasala@gap.com>
2021-09-24 17:03:41 -0300

    removed db connection

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const initRoutes = require('./routes');
 const logger = require('./helpers/logger');
-const { init: initMysql, closeConnection, executeQuery } = require('./helpers/mysql');
+//  const { init: initMysql, closeConnection } = require('./helpers/mysql');
 
 const app = express();
 const port = process.env.PORT;
@@ -14,10 +14,10 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 initRoutes(app);
 
 const init = async () => {
-  if (await initMysql()) {
-    app.listen(port, () => logger.info(`Started server on port ${port}`));
-    console.log(await executeQuery('SELECT 1 + 1 AS solution'));
-  }
+  // if (await initMysql()) {
+  //   app.listen(port, () => logger.info(`Started server on port ${port}`));
+  // }
+  app.listen(port, () => logger.info(`Started server on port ${port}`));
 };
 
 process
@@ -26,7 +26,7 @@ process
   })
   .on('uncaughtException', (err) => {
     logger.error(err, 'Uncaught Exception thrown');
-    closeConnection();
+    //  closeConnection();
     process.exit(1);
   });
 
