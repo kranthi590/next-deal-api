36a435270ec89853f876a7fa52f807f055eb7cf7
Kranthi Kumar Pasala <Kranthi_Kumar_Pasala@gap.com>
2021-10-28 01:05:34 -0300

    changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
+
 const { INVALID_JWT_TOKEN } = require('../helpers/constants');
 const { ForbiddenResponse } = require('../helpers/response.transforms');
+const logger = require('../helpers/logger');
 
 const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET_KEY);
     req.user = decoded;
+    return next();
   } catch (err) {
+    logger.error(err);
     const resp = ForbiddenResponse(INVALID_JWT_TOKEN, req.traceId);
     return res.status(resp.status).json(resp);
   }
-  return next();
 };
 
 module.exports = verifyToken;
