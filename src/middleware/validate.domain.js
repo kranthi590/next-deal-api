0c7e43a773d049821888a0091b1a8806bed384e1
Kranthi Kumar Pasala <Kranthi_Kumar_Pasala@gap.com>
2021-10-28 01:22:03 -0300

    changes

--- src/middleware/validate.domain.js
+++ src/middleware/validate.domain.js
@@ -1,4 +1,4 @@
-const { INVALID_JWT_TOKEN, INVALID_DOMAIN } = require('../helpers/constants');
+const { INVALID_DOMAIN } = require('../helpers/constants');
 const { getSubDomainFromRequest } = require('../helpers/get.subdomain');
 const logger = require('../helpers/logger');
 const { ForbiddenResponse } = require('../helpers/response.transforms');
@@ -14,10 +14,8 @@ const verifyDomain = (req, res, next) => {
     const resp = ForbiddenResponse(INVALID_DOMAIN, req.traceId);
     return res.status(resp.status).json(resp);
   } catch (err) {
-    const resp = ForbiddenResponse(
-      err === INVALID_DOMAIN ? INVALID_DOMAIN : INVALID_JWT_TOKEN,
-      req.traceId,
-    );
+    logger.error(err);
+    const resp = ForbiddenResponse(INVALID_DOMAIN, req.traceId);
     return res.status(resp.status).json(resp);
   }
 };
