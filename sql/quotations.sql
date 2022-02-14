9674558336a3566240e9e8bbe16cef0c5cfeae49
Kranthi Kumar Pasala <pasala.kk@gmail.com>
2022-02-14 16:45:33 +0530

    added commnets

--- sql/quotations.sql
+++ sql/quotations.sql
@@ -1,7 +1,10 @@
 UPDATE `quotation_requests` SET `comments` = `description`;
 ALTER TABLE quotation_requests DROP COLUMN description;
 
-ALTER TABLE quotation_requests MODIFY `estimated_budget` DECIMAL(10,2) NOT NULL;
 ALTER TABLE quotation_requests MODIFY `currency` varchar(50) NOT NULL;
 ALTER TABLE quotation_requests MODIFY `comments` TEXT NOT NULL;
 ALTER TABLE quotation_requests MODIFY `additional_data` TEXT default '';
+
+/*
+    Estimated budget: I cannot make it as NOT NULL
+*/
