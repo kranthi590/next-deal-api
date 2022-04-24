fd4837d0a4a90f6f503c80815d1dee60022585bc
Kranthi Kumar Pasala <pasala.kk@gmail.com>
2022-04-24 11:36:14 -0400

    accept project description and estimated budget as null or empty

--- src/helpers/validations/project-create.js
+++ src/helpers/validations/project-create.js
@@ -8,11 +8,11 @@ const projectCreationSchema = Joi.object()
     startDate: Joi.date().required(),
     expectedEndDate: Joi.date(),
     costCenter: Joi.string(),
-    estimatedBudget: Joi.number(),
+    estimatedBudget: Joi.number().allow(null),
     currency: Joi.string().valid(
       CURRENCY_TYPE.CLP, CURRENCY_TYPE.CL_UF, CURRENCY_TYPE.USD,
     ).required(),
-    description: Joi.string(),
+    description: Joi.string().allow('').allow(null),
   })
   .unknown();
 
