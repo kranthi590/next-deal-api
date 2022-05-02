0ae1bc4ec39c08e07937810ed99b7cfca8422b56
Kranthi Kumar Pasala <pasala.kk@gmail.com>
2022-05-02 19:22:36 -0400

    made deliveryDate required for confirm receptoin

--- src/helpers/validations/confirm-reception.js
+++ src/helpers/validations/confirm-reception.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const confirmReceptionQuotationSchema = Joi.object().keys({
   purchaseOrderNumber: Joi.string().allow('').allow(null),
   comments: Joi.string().allow('').allow(null),
+  deliveryDate: Joi.date().required(),
 }).unknown();
 
 module.exports = { confirmReceptionQuotationSchema };
