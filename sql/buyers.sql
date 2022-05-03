4ea2bca4be42ba0401244d26b659d2fd7b93b86d
Kranthi Kumar Pasala <pasala.kk@gmail.com>
2022-05-02 23:20:54 -0400

    removed sql files

--- sql/buyers.sql
+++
@@ -1,4 +0,0 @@
-Update buyers, projects
-set
-  buyers.created_at = projects.created_at
-WHERE (buyers.id = projects.buyer_id);
--- sql/users.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE users DROP FOREIGN KEY `users_ibfk_1`;
-ALTER TABLE users DROP COLUMN contact_info_id;
-ALTER TABLE users ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE users ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
-
-Update users, projects
-set users.created_at = projects.created_at
-WHERE (users.buyer_id = projects.buyer_id);
